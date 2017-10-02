e8c06667a1b573f7f41ee27e96a0c2a53118f941
Ian R. Taylor <iantay@upenn.edu>
2017-10-02 16:42:34 -0400

    Complete up to the union exercise

--- src/12_take_cover.scm
+++ src/12_take_cover.scm
@@ -64,3 +64,50 @@
             (else
               (cons (car lat) (m-f a (cdr lat))))))))
       m-f)))
+
+; (multirember-f eq?)   => multirember-letrec-3:
+(define multirember-letrec-3
+  (letrec
+    ((mr
+      (lambda (a lat)
+        (cond
+          ((null? lat) (quote ()))
+          ((eq? a (car lat))
+            (mr a (cdr lat)))
+        (else
+          (cons (car lat)
+                (mr a (cdr lat))))))))
+    mr))
+
+; member? checks if a is in lat. note again that a doesn't change
+; during the natural recursions, so we can use the 12th Commandment.
+(define member?
+  (lambda (a lat)
+    (cond
+      ((null? lat) #f)
+      ((eq? a (car lat)) #t)
+    (else (member? a (cdr lat))))))
+
+(define member-letrec?
+  (lambda (a lat)
+    (letrec
+      ((yes?
+          (lambda (l)
+            (cond
+              ((null? l) #f)
+              ((eq? a (car l)) #t)
+            (else
+              (yes? (cdr l)))))))
+      (yes? lat))))
+
+; remember that a set is a list where no atom occurs twice.
+; remember also, the the union function merges too sets into one set.
+; and once again, observe that set2 doesn't change for any of the recursions.
+(define union
+  (lambda (set1 set2)
+    (cond
+      ((null? set1) set2)
+      ((member? (car set1) set2)
+          union (cdr set1) set2))
+    (else (cons (car set1)
+                (union (cdr set1) set2)))))
