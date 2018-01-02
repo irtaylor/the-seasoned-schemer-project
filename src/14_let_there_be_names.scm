8d7834c68e0c403d39a589ef01f9a3b7098fa55b
Ian R. Taylor <iantay@upenn.edu>
2018-01-02 09:39:53 -0500

    Add leftmost and leftmost-2 functions

--- src/14_let_there_be_names.scm
+++ src/14_let_there_be_names.scm
@@ -1,1 +1,27 @@
 ; Chapter 14: Let There Be Names
+
+; leftmost extracts the leftmost atom from a list of S-expr
+(define leftmost
+  (lambda (l)
+    (cond
+      ((atom? (car l)) (car l))
+    (else (leftmost (car l))))))
+
+; (leftmost '(((a) b) (c d))) => 'a
+; (leftmost '(((() a) ()))) => error, b/c leftmost is empty list
+
+; leftmost-2 does not have the above problem
+(define leftmost-2
+  (lambda (l)
+    (cond
+      ((null? l) (quote ()))
+      ((atom? (car l)) (car l))
+    (else
+      (cond
+        ((atom? (leftmost-2 (car l)))
+         (leftmost-2 (car l)))              ; notice here we're needlessly repeating the previous recursions
+      (else (leftmost-2 (cdr l))))))))
+
+; (leftmost-2 '(((a) b) (c d)))
+; (leftmost-2 '(((() a) ())))
+; (leftmost-2 '(((()) ()))) => '()
