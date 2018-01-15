da2996a79018c6d80b30e1296655bd15e8f9b2b0
Ian R. Taylor <iantay@upenn.edu>
2018-01-15 09:23:11 -0500

    Complete rember1* versions

--- src/14_let_there_be_names.scm
+++ src/14_let_there_be_names.scm
@@ -39,4 +39,63 @@
 
 ; (leftmost-let '(((a) b) (c d)))
 ; (leftmost-let '(((() a) ())))
-; (leftmost-let '(((()) ()))) => '()
+; (leftmost-let '(((()) ())))
+
+(define rember1*
+  (lambda (a l)
+    (cond
+      ((null? l) (quote ()))
+      ((atom? (car l))
+        (cond
+          ((eq? (car l) a) (cdr l))
+        (else (cons (car l) (rember1* a (cdr l))))))
+    (else
+      (cond
+        ((equal? (rember1* a (car l))
+          (car l))
+        (cons (car l) (rember1* a (cdr l))))
+      (else (cons (rember1* a (car l))
+              (cdr l))))))))
+
+(rember1* 'salad '((Swedish rye) French (mustard salad turkey) salad))
+
+(define rember1*-letrec
+  (lambda (a l)
+    (letrec
+      ((R (lambda (l)
+            (cond
+              ((null? l) '())
+              ((atom? (car l))
+               (cond
+                 ((eq? (car l) a) (cdr l))
+                 (else
+                   (cons (car l) (R (cdr l))))))
+              (else
+                (cond
+                  ((equal? (R (car l)) (car l)) ; if the list with 'a' removed doesn't change
+                   (cons (car l) (R (cdr l))))  ; then recurse
+                  (else
+                    (cons (R (car l)) (cdr l))))))))) ; otherwise remove 'a'
+      (R l))))
+
+(rember1*-letrec 'salad '((Swedish rye) French (mustard salad turkey) salad))
+
+(define rember1*-let
+  (lambda (a l)
+    (letrec
+      ((R (lambda (l)
+            (cond
+              ((null? l) '())
+              ((atom? (car l))
+               (cond
+                 ((eq? (car l) a) (cdr l))
+                 (else
+                   (cons (car l) (R (cdr l))))))
+              (else
+                (let ((av (R (car l))))
+                  (cond
+                    ((equal? (car l) av)         ; if the list with 'a' removed didn't change
+                     (cons (car l) (R (cdr l)))) ; then recurse
+                    (else
+                      (cons av (cdr l))))))))))  ; otherwise remove 'a'
+      (R l))))
